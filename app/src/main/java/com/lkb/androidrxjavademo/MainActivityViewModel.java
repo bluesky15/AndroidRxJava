5862a412d8a8fc09424568e2c892baea0c2e8003
lkb <lalitkbehera@gmail.com>
2018-08-01 15:14:35 +0530

    Android arch viewModel added

---
+++ app/src/main/java/com/lkb/androidrxjavademo/MainActivityViewModel.java
@@ -0,0 +1,11 @@
+package com.lkb.androidrxjavademo;
+
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+    public String getMessage() {
+        return "Hello world";
+    }
+}
